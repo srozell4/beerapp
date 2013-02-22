a93797192d5c5b6812a5f8b97a660a0572cfd3e6
Scott Rozell <srozell@asu.edu>
2013-02-22 13:24:21 -0700

    test

--- AZBeerSommelier/AppDelegate.m
+++ AZBeerSommelier/AppDelegate.m
@@ -5,7 +5,7 @@
 //  Created by bnander4 on 2/20/13.
 //  Copyright (c) 2013 bnander4. All rights reserved.
 //
-//testing the branch for s
+//test check
 #import "AppDelegate.h"
 
 @implementation AppDelegate
